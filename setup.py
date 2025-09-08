bcf5a24e43eb730c7e413018c14ea065bfa257d2
ERFAN RAD <148450160+ERFUN-RAD@users.noreply.github.com>
2025-09-08 03:40:38 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,30 +5,32 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text(encoding='utf-8')
 
 requirements = [
-    'requests==2.25.1',
-    'requests-toolbelt==0.9.1',
-    'PySocks==1.7.1',
-    'pydantic==1.8.1'
+    'requests>=2.31.0',
+    'requests-toolbelt>=1.0.0',
+    'PySocks>=1.7.1',
+    'pydantic>=1.10.13'
 ]
 
 setup(
-    name='radiojavanapi',
-    version='0.5.0',
-    author='xHossein',
+    name='radiojavanpython',
+    version='0.5.1',
+    author='ERFUN-RAD',
     license='MIT',
-    url='https://github.com/xHossein/radiojavanapi',
+    url='https://github.com/ERFUN-RAD/RADIOJAVANPYTHON',
     install_requires=requirements,
-    keywords=['radiojavan private api','radiojavan-private-api','radiojavan api','radiojavan-api',
-              'rj api','rj-api','radiojavan','radio javan','radio-javan'
+    keywords=[
+        'radiojavan private api', 'radiojavan-private-api', 'radiojavan api',
+        'radiojavan-api', 'rj api', 'rj-api', 'radiojavan', 'radio javan',
+        'radio-javan', 'radiojavanpython'
     ],
-    description='Fast and effective RadioJavan API Wrapper',
+    description='THIS BUG-FREE VERSION OF THE RADIO JAVAN API WRAPPER IS FAST AND RELIABLE',
     long_description=README,
     long_description_content_type='text/markdown',
     packages=find_packages(exclude=['*tests*']),
     python_requires=">=3.7",
     include_package_data=True,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
@@ -36,5 +38,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ]
 )
